127cd6eac5371f5e22dd49822d60963fc1bbd7c2
rrivas <rrivas@utec.edu.pe>
2024-11-22 17:32:04 -0500

    heap updated

--- heap.h
+++ heap.h
@@ -9,9 +9,12 @@
 #include <vector>
 
 namespace utec {
-    template <typename T, template<typename...> typename TContainer = std::vector, typename Comparable = std::less<T>>
+    template <
+            typename T,
+            typename TContainer = std::vector<T>,
+            typename Comparable = std::less<T>>
     class heap {
-        TContainer<T> cnt;
+        TContainer cnt;
         Comparable cmp;
         size_t get_left_child(size_t parent_index) { return parent_index * 2 + 1; }
         size_t get_right_child(size_t parent_index) { return parent_index  * 2 + 2; }
@@ -26,20 +29,77 @@ namespace utec {
             auto right_index = get_right_child(parent_index);
             return right_index < cnt.size();
         }
-        bool get_selected_child_index(size_t parent_index) {
+        size_t get_selected_child_index(size_t parent_index) {
             auto left_index = get_left_child(parent_index);
             if (has_right_child(parent_index)) {
                 auto right_index = get_right_child(parent_index);
-                if (cmp(cnt[right_index], cnt[left_index]))
-//                if (std::less<T>(cnt[right_index], cnt[left_index]))
-//                if (cnt[right_index] < cnt[left_index])
-                    return left_index;
-                else
+                if (cmp(cnt[left_index], cnt[right_index]))
                     return right_index;
-//                if (comparable(cnt[right_index], cnt[left_index]))
-//                    return right_index < cnt.size();
+                else
+                    return left_index;
+            }
+            return left_index;
+        }
+        bool is_root(size_t index) { return index == 0; }
+
+        void percolate_up(size_t index) {
+            // Condiciones básicas
+            if (is_root(index)) return;
+            auto parent_index = get_parent(index);
+            if (cmp(cnt[index], cnt[parent_index])) return;
+            std::swap(cnt[index], cnt[parent_index]);
+            // Condicion recursiva
+            percolate_up(parent_index);
+        }
+        void percolate_down(size_t index) {
+            // condiciones básicas
+            if (!has_children(index)) return;
+            auto selected_child_index = get_selected_child_index(index);
+            if (cmp(cnt[selected_child_index], cnt[index])) return;
+            std::swap(cnt[selected_child_index], cnt[index]);
+            // condicion recursiva
+            percolate_down(selected_child_index);
+        }
+        void heapify(size_t index) {
+            auto current_index = get_parent(index);
+            while (current_index >= 0) {
+                percolate_down(current_index);
+                if (current_index == 0) break;
+                --current_index;
             }
         }
+    public:
+        heap() = default;
+        explicit heap(const TContainer& cnt): cnt(cnt) {
+            heapify(cnt.size() - 1);
+        }
+        explicit heap(const TContainer& cnt, Comparable cmp): cnt(cnt), cmp(cmp) {
+            heapify(cnt.size() - 1);
+        }
+
+        using value_compare = Comparable;
+        using value_type = T;
+        using container_type = TContainer;
+
+        T top() { return cnt.front(); }
+        size_t size() { return cnt.size(); }
+
+        template<typename ...Args>
+        void emplace(Args... args) {
+            cnt.emplace_back(args...);
+            percolate_up(cnt.size() - 1);
+        }
+
+        void push(T value) {
+            cnt.push_back(value);
+            percolate_up(cnt.size() - 1);
+        }
+        void pop() {
+            std::swap(cnt.front(), cnt.back());
+            cnt.pop_back();
+            percolate_down(0);
+        }
+        bool empty() { return cnt.size() == 0; }
     };
 }
 
--- main.cpp
+++ main.cpp
@@ -1,6 +1,55 @@
 #include <iostream>
+#include "heap.h"
+#include <queue>
+#include <stack>
+
+template<typename Type, typename ...OtherTypes>
+struct is_same_to_any {
+    constexpr static bool value = (std::is_same_v<Type, OtherTypes> || ...);
+};
+template<typename Type, typename ...OtherTypes>
+constexpr bool is_same_to_any_v = is_same_to_any<Type, OtherTypes...>::value;
+
+template<typename Type, typename ...OtherTypes>
+struct is_same_to_all {
+    constexpr static bool value = (std::is_same_v<Type, OtherTypes> && ...);
+};
+template<typename Type, typename ...OtherTypes>
+constexpr bool is_same_to_all_v = is_same_to_all<Type, OtherTypes...>::value;
+
+template <typename AdapterType,
+        typename T = typename AdapterType::value_type,
+        typename ContainerT = typename AdapterType::container_type>
+void print_adapter(AdapterType adapter) {
+    while (!adapter.empty()) {
+        if constexpr (is_same_to_any_v<AdapterType, std::queue<T, ContainerT>> == true) {
+            std::cout << adapter.front() << " ";
+        }
+        else if constexpr (is_same_to_any_v<
+                AdapterType,
+                std::stack<T, ContainerT>,
+                std::priority_queue<T, ContainerT, typename AdapterType::value_compare>,
+                utec::heap<T, ContainerT, typename AdapterType::value_compare>
+                > == true){
+            std::cout << adapter.top() << " ";
+        }
+        adapter.pop();
+    }
+    std::cout << std::endl;
+}
+
+
+void ejemplo_1() {
+    std::vector v = {12, 1, 7, 15, 2, 20};
+    utec::heap<int> h1(v);
+    print_adapter(h1);
+    std::cout << std::endl;
+
+
+
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    ejemplo_1();
     return 0;
 }
